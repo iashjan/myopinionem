f07b5e15541490248556e1a7a55ce5c91d0f301c
iashjan <ashjanot@gmail.com>
2019-12-19 10:20:46 +0300

    project 4 done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  get '/books/show/:id/delete' => 'books#destroy'
+
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  resources :books do
+    resources :reviews
+  end
+
+    root 'books#index'
+
 end
